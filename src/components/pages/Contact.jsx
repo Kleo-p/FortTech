28ed7f1b5f3e0a196c1dd31a8345ba8393e2ef5b
Kleo-p <kleovader@gmail.com>
2023-03-06 13:50:59 +0100

    made it single page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,11 +1,66 @@
-import React from 'react';
+import React from "react";
 
 export default function Contact() {
-    return (
+  return (
+    <div>
+      <div>
+        <h1>Contact us</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+          aliquam optio ab eum. Ex, quas aperiam ipsum dignissimos perspiciatis
+          quia.
+        </p>
         <div>
-            <div>
-                <h1>Contact</h1>
-            </div>
+          <p>08012345678, 08012345678</p>
         </div>
-    )
-};
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo dicta
+            officia consectetur eius nesciunt cumque.
+          </p>
+        </div>
+        <div className="social cont">
+          <div id="simages">
+            <ul>
+              <li>
+                <a href="#">
+                  <img src="./facebook icon.png" alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="./google icon.png" alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="./Instagram_icon.png.webp" alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="./linkedin icon.png" alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="./Twitter-logo.svg.webp" alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="./whatsap icon.png" alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="./IMG_1956.PNG" alt="" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
